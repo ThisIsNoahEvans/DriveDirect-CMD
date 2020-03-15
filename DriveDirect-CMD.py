30ebe9da08a9dcc57081b82d3785748746d0a04f
powerdudeg4 <62171400+powerdudeg4@users.noreply.github.com>
2020-03-15 21:18:11 +0100

    Removing Pandas, using pyperclip instead.
    Pandas is a little too much IMHO and does not come preinstalled. pyperclip serves one function and is now mentioned in the README

--- DriveDirect-CMD.py
+++ DriveDirect-CMD.py
@@ -1,17 +1,16 @@
-import pandas as pd
-
-#Print Welcome statement
 print('Welcome to the Google Drive Direct Download Link Generator!')
-#Get URL
+
 link = input('Please paste in your URL: ')
-#Create downloadLink and make a string
+
 downloadLink = str('')
-#Create direct URL
+
 downloadLink = link.replace('file/d/', 'uc?export=download&id=')
 downloadLink = downloadLink.replace('/view?usp=sharing', '')
-#Print URL
+
 print('Your URL is', downloadLink)
+
 #Copy to clipboard
-df = pd.DataFrame([downloadLink])
-df.to_clipboard(index=False, header=False)
+import pyperclip
+pyperclip.copy(downloadLink)
+pyperclip.paste()
 print('Your URL has been copied to your clipboard.')
